1ffeac09c5d0e0e4199588ff756bab3ec19326f3
AbrahamV <abraham.varricatt@googlemail.com>
2016-09-09 00:34:29 +0530

    update deploy path

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.insert(0, '/srv/sources/my-polls')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
